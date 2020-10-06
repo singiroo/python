2e0947286bd63005595ddaea30e6a3b7d2677b64
202005_kyh <linkan.kim@gmail.com>
2020-10-06 21:44:06 +0900

    20201006 21:43 python - sqlite3 연동 crud 작업 테트리스 기본 로직 및 밑준비 작업 진행중

--- HelloPython/day08/SqliteInsert.py
+++ HelloPython/day08/SqliteInsert.py
@@ -2,11 +2,15 @@ import sqlite3
 
 print("sqlite3")
 
-conn = sqlite3.connect("mydb.db")
+#Autocommit 사용자:
+
+conn = sqlite3.connect("mydb.db", isolation_level=None)
+
 cursor = conn.cursor()
 sql = "insert into mytable values(?, ?, ?)"
 data = ('5', '6', '7')
 cursor.execute(sql,data)
 
-conn.commit()
+#conn.commit()
+
 conn.close()
